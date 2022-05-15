8f1de0a2026a1ea584558d2d4a3169819b34de19
Natsu Ozawa <natsuozawa@outlook.com>
2022-05-15 19:56:29 +0100

    Add authentication

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Monitors a OneDrive directory for uploads of new CSV and DB files.
 
+This app only supports storing credentials for one user.
+
 # Virtual environment
 
 This app uses Python's virtual environment feature. To enable:
@@ -16,4 +18,32 @@ Install dependencies.
 
 ```
 pip3 install -r requirements.txt
+```
+
+# Azure Active Directory app registration
+This app needs to be registered on Azure Active Directory. In order to use personal accounts, select the option that allows all users to use the service.
+
+## Generate client secret
+Generate a client secret from the certificates and secrets section. Copy this into the configuration as shown below.
+
+## Add permissions
+Add a permission in Azure Active Directory from the API permissions section. This app requires the following delegated Microsoft Graph API permissions to be added to the app.
+
+```
+offline_acess
+Files.Read
+User.Read
+```
+
+# Configuration
+
+This app requires the following environment variables to be configured. 
+
+The following are variables related to the Microsoft API. See the [documentation](https://docs.microsoft.com/en-us/graph/auth-v2-user) for more details.
+
+```
+TENANT = "common", "organizations", or "consumers"
+APPLICATION_ID = corresponds to client_id, obtained from Azure Active Directory
+REDIRECT_URL = url of the app, corresponds to redirect_uri
+CLIENT_SECRET = client secret registered in the Azure Active Directory.
 ```
--- app.py
+++ app.py
@@ -1,10 +1,99 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+
+import os
 
 app = Flask(__name__)
 app.config.from_pyfile('app.cfg')
 
-@app.route("/")
+@app.route('/')
 def index():
-    scope = "%20".join(['offline_access', 'files.read'])
-    # return "true"
-    return render_template('index.html', application_id=app.config['APPLICATION_ID'], redirect_url=app.config['REDIRECT_URL'], scope=scope) 
+    if read_tokens():
+        request_tokens(refresh=True)
+        return redirect(url_for('update_files'))
+
+    return render_template('index.html', tenant=app.config['TENANT'], application_id=app.config['APPLICATION_ID'], redirect_url=app.config['REDIRECT_URL'], scope=permission_scope()) 
+
+@app.route('/register_token')
+def register_token():
+    status, err = request_tokens(refresh=False, code=request.args.get('code', type=str))
+    if status:
+        return redirect(url_for('update_files'))
+    return err['error_description']
+
+@app.route('/update_files')
+def update_files():
+    return 'Work in progress.'
+
+"""
+Send a POST request to obtain access and refresh tokens from Microsoft.
+code: 
+"""
+def request_tokens(refresh=False, code=''):
+    url = 'https://login.microsoftonline.com/' + app.config['TENANT'] + '/oauth2/v2.0/token'
+    data = {
+        'client_id': app.config['APPLICATION_ID'], 
+        'scope': permission_scope(include_offline_acess=False),
+        'grant_type': 'refresh_token' if refresh else 'authorization_code',
+        'redirect_uri': app.config['REDIRECT_URL'],
+        'client_secret': app.config['CLIENT_SECRET']
+    }
+
+    if refresh:
+        data['refresh_token'] = app.config['REFRESH_TOKEN']
+    else:
+        data['code'] = code
+
+    r = requests.post(url, data = data)
+    res = r.json()
+
+    if 'error' in res:
+        return False, res
+
+    app.config['ACCESS_TOKEN'] = res['access_token']
+
+    if 'refresh_token' in res:
+        app.config['REFRESH_TOKEN'] = res['refresh_token']
+
+    write_tokens()
+    return True, None
+
+"""
+Create permission scope string.
+"""
+def permission_scope(include_offline_acess=True):
+    permissions = ['files.read', 'user.read']
+    if include_offline_acess:
+        permissions.append('offline_acess')
+    return "%20".join(permissions)
+
+"""
+Read refresh and access tokens from the refresh_token and access_token files.
+If they do not exist or are empty, return False. 
+Otherwise, read the tokens into the configuration files and return True.
+"""
+def read_tokens():
+    if 'refresh_token' in os.listdir() and 'access_token' in os.listdir():
+        with open('refresh_token', 'r') as f:
+            t = f.readline()
+            if t:
+                app.config['REFRESH_TOKEN'] = t
+            else:
+                return False
+        with open('access_token', 'r') as f:
+            t = f.readline()
+            if t:
+                app.config['ACCESS_TOKEN'] = t
+            else:
+                return False
+        return True
+    return False
+
+"""
+Write refresh and access tokens into the refresh_token and access_token files.
+"""
+def write_tokens():
+    with open('refresh_token', 'w+') as f:
+        f.write(app.config['REFRESH_TOKEN'])
+    with open('access_token', 'w+') as f:
+        f.write(app.config['ACCESS_TOKEN'])
