bf86496471cc499b2e3d294874a3bc6b28a43806
Natsu Ozawa <natsuozawa@outlook.com>
2022-07-10 13:26:45 +0900

    Prepare for production

--- index.py
+++ index.py
@@ -4,4 +4,7 @@ from tokens import *
 from files import *
 
 if __name__ == '__main__':
+  if app.config["MDOE"] == "debug":
     app.run(host="0.0.0.0", port=5000, debug=True)
+  else:
+    app.run(host="0.0.0.0", port=5000)
