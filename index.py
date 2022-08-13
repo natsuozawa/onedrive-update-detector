02d28866a3be3b2750141af75a00efd91e790128
Natsu Ozawa <natsuozawa@outlook.com>
2022-08-13 23:12:54 +0900

    Fix production mode

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ from waitress import serve
 if __name__ == '__main__':
   if app.config["MODE"] == "debug":
     app.run(host="0.0.0.0", port=5000, debug=True)
-  elif app.config["MODE"] == "development":
+  elif app.config["MODE"] == "production":
     serve(app, host="0.0.0.0", port=5000)
   else:
     app.run(host="0.0.0.0", port=5000)
